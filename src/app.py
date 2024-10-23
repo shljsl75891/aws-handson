2ff789dbf936c523a866720816bea96332a4b62b
shljsl75891 <shljsl75891@gmail.com>
2024-10-23 06:02:21 +0530

    feat: API Gateway as Lambda proxy integration

--- src/app.py
+++ src/app.py
@@ -1,4 +1,18 @@
+import boto3
+import json
+import os
+
 print('Loading function')
 
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
 def lambda_handler(event, context):
-    return "Hello from Serverless Application Model Handler"
+    return respond(None, "Hello From SAM")
